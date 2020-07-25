909956bcc7143ec8ebac028e2cc20d4370e197d7
Alfred_Ametepey <alfredtetteh726@gmail.com>
2020-07-25 10:53:11 +0000

    second commit

--- 1/question4.sql
+++ 1/question4.sql
@@ -1,5 +1,6 @@
 select extract(year from when_created) as yr, extract(month from when_created) as mon,
        count(atx_id)
 from agent_transactions
+where when_created between '01-01-2018' and '31-12-2018'
 group by extract(year from when_created), extract(month from when_created)
 order by yr, mon;
